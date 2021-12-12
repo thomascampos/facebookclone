4cce711f6f2e873c2d5d670476b9ef374c6f1a2d
Thomas <thomas.campos.31@gmail.com>
2021-12-12 20:46:44 +0100

    [ORDER] Init Project Facebook Clone

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Facebook</title>
+      </Head>
+
+       <Header/>
+
+      <main>
+        {/* SideBar */}
+        {/* Feed */}
+        {/* Widget */}
+      </main>
+     
+    </div>
+  )
+}
